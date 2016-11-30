6af6dcfa45438199d4cef2bbb44f13ee3d2544f2
dan-j <jonesd9@me.com>
2016-11-30 20:22:11 +0000

    Added git-crypt with encrypted config.js

--- server/config-example.js
+++ server/config-example.js
@@ -1,9 +1,20 @@
-exports.default = {
-  db: {
+let mongoConfig;
+
+if (process.env.NODE_ENV === 'production') {
+  mongoConfig = {
     url: 'host:port/dbname',
     user: 'user',
     pass: 'changeme',
-  },
+  };
+} else {
+  mongoConfig = {
+    url: 'localhost/dbname',
+  };
+}
+
+
+exports.default = {
+  db: mongoConfig,
 
   jwt: {
     secretOrKey: 'mysecret',
