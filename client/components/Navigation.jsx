b9e44def5c411c4221ac64350e4223e4d4abfc79
dan-j <jonesd9@me.com>
2016-12-15 21:03:40 +0000

    - Made responsive navigation bar

--- client/components/Navigation.jsx
+++ client/components/Navigation.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router';
+import FontAwesome from 'react-fontawesome';
+import classNames from 'classnames';
 import { isLoggedIn } from '../containers/LoginContainer';
 
-const NavItem = ({ to, children, className }) => (
+const linkStyle = { padding: '0.5em', margin: '0 0.5em', display: 'inline-block' };
+
+const NavItem = ({ to, children, className, onClick }) => (
   <li
     style={{
       display: 'inline-block',
@@ -11,10 +15,11 @@ const NavItem = ({ to, children, className }) => (
     }}
   >
     <Link
-      style={{ padding: '0.5em 0.5em 0.25em', margin: '0 0.5em', display: 'inherit' }}
+      style={linkStyle}
       activeClassName="active"
       onlyActiveOnIndex
       to={to}
+      onClick={onClick}
       className={className}
     >{children}</Link>
   </li>
@@ -24,36 +29,77 @@ NavItem.propTypes = {
   to: React.PropTypes.string,
   children: React.PropTypes.node.isRequired,
   className: React.PropTypes.string,
+  onClick: React.PropTypes.func,
 };
 
-const Navigation = ({ color }) => {
-  const loginLogoff = isLoggedIn()
-    ? <NavItem className="logout" to="/logout">Logout</NavItem>
-    : <NavItem className="login" to="/login">Login</NavItem>;
+export default class Navigation extends React.Component {
 
-  return (
-    <div style={{ textAlign: 'right', color, zIndex: 2, position: 'relative', right: '0.5em' }}>
-      <ul style={{ margin: 0 }}>
-        <NavItem to="/">Home</NavItem>
-        <NavItem to="/invitations">Invitations</NavItem>
-        <NavItem>Registry</NavItem>
-        <NavItem>More Info</NavItem>
-        {loginLogoff}
-      </ul>
-    </div>
-  );
-};
+  static contextTypes = {
+    router: React.PropTypes.object,
+  };
 
-Navigation.contextTypes = {
-  router: React.PropTypes.object,
-};
+  static propTypes = {
+    color: React.PropTypes.string,
+  };
 
-Navigation.propTypes = {
-  color: React.PropTypes.string,
-};
+  static defaultProps = {
+    color: 'white',
+  };
 
-Navigation.defaultProps = {
-  color: 'white',
-};
+  constructor(props) {
+    super(props);
 
-export default Navigation;
+    this.state = {
+      burgerPressed: false,
+    };
+
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
+
+  toggleMenu() {
+    this.setState({ burgerPressed: !this.state.burgerPressed });
+  }
+
+  render() {
+    const loginLogoff = isLoggedIn()
+      ? <NavItem className="logout" to="/logout">Logout</NavItem>
+      : <NavItem className="login" to="/login">Login</NavItem>;
+
+    let { color } = this.props;
+
+    let background;
+
+    if (this.state.burgerPressed) {
+      background = 'black';
+      color = 'white';
+    }
+
+    const baseDivStyle = {
+      maxWidth: 1170, margin: 'auto', textAlign: 'right', color, zIndex: 3, position: 'relative',
+    };
+
+    const listClassNames = classNames('nav-menu', { 'nav-menu-open': this.state.burgerPressed });
+    const divStyle = this.state.burgerPressed
+      ? { ...baseDivStyle, background }
+      : baseDivStyle;
+
+    return (
+      <div className="navigation" style={divStyle}>
+        <a
+          className="nav-burger"
+          style={{ ...linkStyle, fontSize: '2em', margin: '0', color }}
+          onClick={this.toggleMenu}
+        >
+          <FontAwesome name="bars" />
+        </a>
+        <ul className={listClassNames} style={{ margin: 0 }}>
+          <NavItem to="/" onClick={this.state.burgerPressed && this.toggleMenu}>Home</NavItem>
+          <NavItem to="/invitations" onClick={this.state.burgerPressed && this.toggleMenu}>Invitations</NavItem>
+          <NavItem>Registry</NavItem>
+          <NavItem>More Info</NavItem>
+          {loginLogoff}
+        </ul>
+      </div>
+    );
+  }
+}
