54e0d8d635ed90c00d8ece11655c561357e9217c
dan-j <jonesd9@me.com>
2016-12-21 18:28:04 +0000

    - InvitationsContainer no longer fetches all invitations

--- client/containers/InvitationsContainer.jsx
+++ client/containers/InvitationsContainer.jsx
@@ -22,25 +22,17 @@ export default class InvitationsContainer extends React.Component {
   loadData() {
     const { token } = localStorage;
     if (token) {
-      Promise.all([
-        api.getMe(token),
-        api.getUsers(token),
-      ]).then(values => {
-        const [me, users] = values;
-        this.setState({
-          user: {
-            name: me.name,
-            partySize: me.partySize,
-            ...me.invitation,
-          },
-          data: users.map(item => ({
-            name: item.name,
-            partySize: item.partySize,
-            ...item.invitation,
-          })),
-          loaded: true,
-        });
-      }).catch(err => err.response.json())
+      api.getMe(token)
+        .then(me => {
+          this.setState({
+            user: {
+              name: me.name,
+              partySize: me.partySize,
+              ...me.invitation,
+            },
+            loaded: true,
+          });
+        }).catch(err => err.response.json())
         .catch(json => this.setState({
           error: json.message,
         }));
@@ -51,7 +43,6 @@ export default class InvitationsContainer extends React.Component {
     return this.state.loaded
       ? <Invitations
         myInvitation={this.state.user}
-        otherInvitations={this.state.data}
         onLogoff={this.handleLogoff}
       />
       : <h2>Loading</h2>;
