697bb54e869ce92fd55722bb30f6f252f82dbeaf
dan-j <jonesd9@me.com>
2017-01-24 00:17:01 +0000

    - (actually) fixed eslint issues

--- client/components/Invitations/common.js
+++ client/components/Invitations/common.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
-const invitationShape = React.PropTypes.shape({
+export const invitationShape = React.PropTypes.shape({
   ceremony: React.PropTypes.bool,
   meal: React.PropTypes.bool,
   evening: React.PropTypes.bool,
   friendlyName: React.PropTypes.string,
   partySize: React.PropTypes.number,
 });
-
-export default { invitationShape };
 
