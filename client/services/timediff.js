8b56ad888be76486a3ac88485ae1826c670278d3
dan-j <jonesd9@me.com>
2017-05-17 06:53:10 +1000

    Fixed countdown bug now the wedding has passed

--- client/services/timediff.js
+++ client/services/timediff.js
@@ -4,10 +4,18 @@ const SECONDS_IN_DAY = 24 * SECONDS_IN_HOUR;
 
 export default (now, then) => {
   const diff = parseInt((then - now) / 1000, 10);
-  const days = Math.floor(diff / SECONDS_IN_DAY);
-  const hours = Math.floor(diff / SECONDS_IN_HOUR) % 24;
-  const minutes = Math.floor(diff / SECONDS_IN_MINUTE) % 60;
-  const seconds = Math.floor(diff % 60);
+
+  let days = 0;
+  let hours = 0;
+  let minutes = 0;
+  let seconds = 0;
+
+  if (diff > 0) {
+    days = Math.floor(diff / SECONDS_IN_DAY);
+    hours = Math.floor(diff / SECONDS_IN_HOUR) % 24;
+    minutes = Math.floor(diff / SECONDS_IN_MINUTE) % 60;
+    seconds = Math.floor(diff % 60);
+  }
 
   return { days, hours, minutes, seconds };
 };
